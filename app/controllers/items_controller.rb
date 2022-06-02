3c3a0be524e09aa07f3e7b251efac58b0a4f0778
kuriyama585 <kuriyama585@yahoo.co.jp>
2022-06-02 17:46:31 +0900

    商品詳細表示機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,10 @@ def create
     end
 end
 
+def show
+  @item = Item.find(params[:id])
+end
+
 private
   def item_params
     params.require(:item).permit(:item_name, :item_text, :category_id,:situation_id,:delivery_charge_id,:prefecture_id,:delivery_date_id,:price,:image).merge(user_id: current_user.id)
