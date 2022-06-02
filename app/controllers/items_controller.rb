15f2a9b3c82a45d34bc1c1e99519905f7d58f8fa
kuriyama585 <kuriyama585@yahoo.co.jp>
2022-06-02 22:26:08 +0900

    商品情報編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,only: :new
+  before_action :authenticate_user!,only: [:new ,:edit ,:destroy]
 
 def index
   @item = Item.includes(:user).order("created_at DESC")
@@ -23,6 +23,24 @@ end
 
 def show
   @item = Item.find(params[:id])
+end
+
+def edit
+  @item = Item.find(params[:id])
+  unless  current_user.id == @item.user_id
+    redirect_to action: :index
+  end
+  # ↑ログインかつ投稿者のみ遷移可能の条件
+  # 課題はbefore_actionで条件をつけれるようにする
+end
+
+def update
+  @item = Item.find(params[:id])
+  if @item.update(item_params)
+    redirect_to item_path
+  else
+    render :edit
+  end
 end
 
 private
